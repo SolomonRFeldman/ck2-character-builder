ddbaf9c122eca5b54a67ef5e76d196ea84c6e27e
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-11-04 16:09:55 -0500

    builds basic card with attributes

--- ck2-character-builder-frontend/src/index.js
+++ ck2-character-builder-frontend/src/index.js
@@ -3,3 +3,4 @@ import Character from './models/Character.js';
 const mainBody = document.querySelector("main")
 
 const character = new Character;
+mainBody.append(character.buildCard());
--- ck2-character-builder-frontend/src/models/Character.js
+++ ck2-character-builder-frontend/src/models/Character.js
@@ -10,6 +10,26 @@ class Character {
   constructor(attributes = DEFAULT_ATTR, traits) {
     this.attributes = attributes
   };
+
+  buildCard() {
+    const card = document.createElement("div")
+    card.setAttribute("style", "width: 800px;")
+    card.setAttribute("class", "card mx-auto my-4")
+    card.append(this.buildAttrs())
+    return card
+  };
+
+  buildAttrs() {
+    const list = document.createElement("ul");
+    list.setAttribute('class', 'list-group');
+    for(const attr in this.attributes) {
+      const item = document.createElement("li");
+      item.setAttribute('class', 'list-group-item');
+      item.innerHTML = `${attr[0].toUpperCase() + attr.slice(1)}: <div class="float-right">${this.attributes[attr]}</div>`;
+      list.append(item);
+    }
+    return list
+  }
 };
 
 export default Character;
