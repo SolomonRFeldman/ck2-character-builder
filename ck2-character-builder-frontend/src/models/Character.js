d130722b894f21094a145e7d538a42f2c076336c
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-11-04 20:03:19 -0500

    adds plus/minus buttons and attr value

--- ck2-character-builder-frontend/src/models/Character.js
+++ ck2-character-builder-frontend/src/models/Character.js
@@ -6,6 +6,14 @@ const DEFAULT_ATTR = {
   learning: 5
 };
 
+const ATTRIBUTE_INCREMENT = {
+  diplomacy: 1,
+  martial: 1,
+  stewardship: 1,
+  intrigue: 1,
+  learning: 1,
+}
+
 class Character {
   constructor(attributes = DEFAULT_ATTR, traits) {
     this.attributes = attributes;
@@ -39,9 +47,36 @@ class Character {
   buildAttrValue(attr) {
     const value = document.createElement('div');
     value.setAttribute('class', 'float-right');
-    value.innerText = this.attributes[attr];
+    value.innerHTML = `<span id="base">${this.attributes[attr]}</span>`;
+    this.appendPlusMinusButtons(attr, value);
     return value;
+  };
+
+  appendPlusMinusButtons(attr, target) {
+    const plus = document.createElement('button');
+    plus.setAttribute('class', 'btn btn-success btn-sm mx-2');
+    plus.innerText = '➕';
+    target.append(plus);
+    plus.addEventListener('click', () => { this.plusMinusAttr(attr, target, 1) }); 
+
+    const minus = document.createElement('button');
+    minus.setAttribute('class', 'btn btn-danger btn-sm');
+    minus.innerText = '➖';
+    target.append(minus);
+    minus.addEventListener('click', () => { 
+      if (parseFloat(target.querySelector('#base').innerText) > 5) {
+        this.plusMinusAttr(attr, target, -1);
+      };
+    }); 
   }
+
+  plusMinusAttr(attr, target, direction) {
+    const base = target.querySelector('#base');
+    let newVal = parseFloat(base.innerText) + (ATTRIBUTE_INCREMENT[attr] * direction);
+    if (newVal % 1 !== 0) { newVal = newVal.toFixed(2) };
+    base.innerText = newVal
+    this[attr] = newVal
+  };
 };
 
 export default Character;
