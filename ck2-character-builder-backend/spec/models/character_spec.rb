358880f98c1e74d0ec7877160575cb63a2e9e563
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-11-07 20:54:11 -0500

    adds character serializer test

--- ck2-character-builder-backend/spec/models/character_spec.rb
+++ ck2-character-builder-backend/spec/models/character_spec.rb
@@ -6,14 +6,24 @@ RSpec.describe Character, :type => :model do
       name: "Ragnarr",
       dynasty: "Loðbrók",
       marriage_status: true,
-      religion: "Germanic",
       culture: "Norse",
+      religion: "Germanic",
       sex: "Male"
     }
   end
 
   it "is valid with a name, dynasty, marriage_status, religion, culture, and sex" do
     expect(Character.create(valid_character)).to be_valid
+  end
+
+  context "when a valid character has been created" do
+    before do
+      Character.create(valid_character)
+    end
+
+    it "can be serialized" do
+      expect(CharacterSerializer.new(Character.all.last).to_serialized_json).to eq(Character.all.last.to_json)
+    end
   end
 
 end
