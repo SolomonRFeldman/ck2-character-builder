9a22a44e7193e5ad14249200aa125e3b0f7a03ad
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-11-07 21:43:54 -0500

    adds create request for existing char test

---
+++ ck2-character-builder-backend/spec/features/characters/edit_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'Character Features', :type => :feature do
+  let(:valid_character) do
+    {
+      id: nil,
+      name: "Ragnarr",
+      dynasty: "Loðbrók",
+      marriage_status: true,
+      culture: "Norse",
+      religion: "Germanic",
+      sex: "Male"
+    }
+  end
+
+  context "when a post request for a already saved character is sent" do
+    before do
+      Character.create(valid_character)
+      page.driver.submit :post, characters_path, character: valid_character.merge({ id: Character.all.last.id, religion: "Catholic" })
+    end
+
+    it "patches the character" do
+      expect(Character.all.last.religion).to eq("Catholic")
+    end
+  end
+  
+end
