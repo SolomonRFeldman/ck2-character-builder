819eb05e096f90b952064149fcdfa6387e379a8f
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-11-09 19:24:32 -0500

    adds attribute setting test

--- ck2-character-builder-backend/spec/features/characters/create_spec.rb
+++ ck2-character-builder-backend/spec/features/characters/create_spec.rb
@@ -9,17 +9,24 @@ describe 'Character Features', :type => :feature do
       marriage_status: true,
       culture: "Norse",
       religion: "Germanic",
-      sex: "Male"
+      sex: "Male",
+      character_attribute: {
+        diplomacy: 5,
+        martial: 5,
+        stewardship: 5,
+        intrigue: 5,
+        learning: 5,
+        health: 5,
+        fertility: 50,
+        sons: 0,
+        daughters: 0
+      }
     }
-  end
-
-  let(:valid_character_attribute) do
-
   end
 
   context "when a post request for a new character is sent with an attribute set" do
     before do
-      page.driver.submit :post, characters_path, character: valid_character.merge({ character_attribute: valid_character_attribute })
+      page.driver.submit :post, characters_path, character: valid_character
     end
 
     it "creates the character" do
@@ -28,6 +35,11 @@ describe 'Character Features', :type => :feature do
 
     it "returns the character jsonified" do
       expect(page).to have_content(CharacterSerializer.new(Character.all.last).to_serialized_json)
+    end
+
+    it "creates a character with an attribute set" do
+      expect(Character.all.last.character_attribute).to_not be_nil
+      expect(Character.all.last.character_attribute).to eq(CharacterAttribute.all.last)
     end
   end
 
