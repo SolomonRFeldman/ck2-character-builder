658b475cd602a7b43a66306972113027bd99c4f1
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-11-10 13:08:49 -0500

    adds test for characters index show

---
+++ ck2-character-builder-backend/spec/features/characters/show_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe 'Character Features', :type => :feature do
+
+  let(:valid_character) do
+    {
+      id: nil,
+      name: "Ragnarr",
+      dynasty: "Loðbrók",
+      marriage_status: true,
+      culture: "Norse",
+      religion: "Germanic",
+      sex: "Male",
+      character_attribute: {
+        diplomacy: 5,
+        martial: 5,
+        stewardship: 5,
+        intrigue: 5,
+        learning: 5,
+        health: 5,
+        fertility: 50,
+        sons: 0,
+        daughters: 0
+      }
+    }
+  end
+
+  context "when a get request is sent to /characters" do
+    before do
+      page.driver.submit :post, characters_path, character: valid_character
+      page.driver.submit :get, characters_path, {}
+    end
+
+    it "serializes all the characters" do
+      expect(page).to have_content(CharacterSerializer.new(Character.all.last).to_serialized_json)
+    end
+  end
+
+end 
