e6275a8ee7d61fe3f47ae1feb24b9eb3430fd1b7
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-11-09 23:09:29 -0500

    hash maps all cultures according to group

---
+++ ck2-character-builder-backend/app/models/culture.rb
@@ -0,0 +1,205 @@
+class Culture
+
+  class << self
+    def all
+      {
+        "North Germanic": [
+          "Norse",
+          "Swedish",
+          "Norwegian",
+          "Danish"
+        ],
+
+        "Central Germanic": [
+          "German",
+          "Lombard",
+          "Frankish",
+          "Suebi"
+        ],
+
+        "West Germanic": [
+          "English",
+          "Anglo-Saxon",
+          "Saxon",
+          "Frisian",
+          "Dutch"
+        ],
+
+        "Latin": [
+          "French",
+          "Norman",
+          "Italian",
+          "Occitan",
+          "Roman",
+          "Dalmatian",
+          "Outremer",
+          "Sardinian"
+        ],
+
+        "Iberian": [
+          "Basque",
+          "Castilian",
+          "Catalan",
+          "Portuguese",
+          "Visigothic"
+        ],
+
+        "Byzantine": [
+          "Arberian",
+          "Armenian",
+          "Greek",
+          "Alan",
+          "Georgian",
+          "Assyrian",
+          "Coptic",
+          "Gothic"
+        ],
+
+        "Celtic": [
+          "Irish",
+          "Scottish",
+          "Pictish",
+          "Welsh",
+          "Breton"
+        ],
+
+        "Finno-Ugric": [
+          "Finnish",
+          "Sami",
+          "Estonian",
+          "Komi",
+          "Khanty",
+          "Nenets",
+          "Mordvin",
+          "Meshchera"
+        ],
+
+        "Baltic": [
+          "Lettigallian",
+          "Lithuanian",
+          "Prussian"
+        ],
+
+        "Altaic": [
+          "Turkish",
+          "Pecheneg",
+          "Cuman",
+          "Khazar",
+          "Bolghar",
+          "Avar",
+          "Karluk",
+          "Kirghiz",
+          "Uyghur",
+          "Mongol",
+          "Khitan",
+          "Jurchen"
+        ],
+
+        "Arabic": [
+          "Bedouin",
+          "Berber",
+          "Levantine",
+          "Egyptian",
+          "Andalusian",
+        ],
+
+        "East Slavic": [
+          "Russian",
+          "Ilmenian",
+          "Severian",
+          "Volhynian"
+        ],
+
+        "West Slavic": [
+          "Pomeranian",
+          "Bohemian",
+          "Polish",
+          "Slovien"
+        ],
+
+        "South Slavic": [
+          "Croatian",
+          "Serbian",
+          "Vlach",
+          "Bulgarian",
+          "Bosnian",
+          "Carantanian"
+        ],
+
+        "Magyar": [
+          "Hungarian"
+        ],
+
+        "Iranian": [
+          "Persian",
+          "Sogdian",
+          "Tocharian",
+          "Kurdish",
+          "Afghan",
+          "Baloch",
+          "Saka"
+        ],
+
+        "East African": [
+          "Ethiopian",
+          "Somali",
+          "Nubian",
+          "Daju"
+        ],
+
+        "Central African": [
+          "Kanuri",
+          "Hausa",
+          "Zaghawa"
+        ],
+
+        "West African": [
+          "Mandé",
+          "Soninke",
+          "Songhey"
+        ],
+
+        "Mesoamerican": [
+          "Nahua"
+        ],
+
+        "Israelite": [
+          "Ashkenazi",
+          "Sephardi"
+        ],
+
+        "Indo-Aryan": [
+          "Bengali",
+          "Oriya",
+          "Assamese",
+          "Hindustani",
+          "Gujarati",
+          "Panjabi",
+          "Rajput",
+          "Sindhi",
+          "Marathi",
+          "Sinhala",
+          "Nepali"
+        ],
+
+        "Dravidian": [
+          "Tamil",
+          "Telugu",
+          "Kannada"
+        ],
+
+        "Tibeto-Burman": [
+          "Bödpa",
+          "Tangut",
+          "Zhangzhung",
+          "Sumpa"
+        ],
+
+        "Chinese": [
+          "Han"
+        ]
+      }
+    end
+  end
+
+end
