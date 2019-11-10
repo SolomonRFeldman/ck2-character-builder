24371738c828986c23799085df330e2c7831c280
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-11-09 19:24:36 -0500

    adds test for patching characters attributes

--- ck2-character-builder-backend/spec/features/characters/edit_spec.rb
+++ ck2-character-builder-backend/spec/features/characters/edit_spec.rb
@@ -24,16 +24,37 @@ describe 'Character Features', :type => :feature do
     }
   end
 
+  let(:valid_character_attribute) do
+    {
+      diplomacy: 5,
+      martial: 5,
+      stewardship: 5,
+      intrigue: 5,
+      learning: 5,
+      health: 5,
+      fertility: 50,
+      sons: 0,
+      daughters: 0
+    }
+  end
+
   context "when a post request for a already saved character is sent" do
     before do
       page.driver.submit :post, characters_path, character: valid_character
-      page.driver.submit :post, characters_path, character: valid_character.merge({ id: Character.all.last.id, religion: "Catholic" })
+      page.driver.submit :post, characters_path, character: valid_character.merge({ 
+        id: Character.all.last.id,
+        religion: "Catholic",
+        character_attribute: valid_character_attribute.merge({ diplomacy: 10 })
+      })
     end
 
-    it "patches the character" do
+    it "patches the character's direct info" do
       expect(Character.all.last.religion).to eq("Catholic")
     end
 
+    it "patches the character's attributes" do
+      expect(Character.all.last.character_attribute.diplomacy).to eq(10)
+    end
   end
 
 end
