e5e6f943b64e3d591f6fd3fc879cb930520f751e
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-11-09 19:29:21 -0500

    adds posting attributes for creating/patching

--- ck2-character-builder-backend/app/controllers/characters_controller.rb
+++ ck2-character-builder-backend/app/controllers/characters_controller.rb
@@ -1,16 +1,29 @@
 class CharactersController < ApplicationController
 
   def create
-    character = Character.find_by(id: params[:character][:id])
-    character ? character.update(character_params) : character = Character.new(character_params)
-    character.save
-    render json: CharacterSerializer.new(character).to_serialized_json
+    character = Character.find_or_create_with_attributes(character_params)
+    render json: CharacterSerializer.new(character).to_serialized_json if character.save
   end
 
   private
 
+  def character_attribute_params
+    { character_attribute: [:diplomacy, :martial, :stewardship, :intrigue, :learning, :health, :fertility, :sons, :daughters] }
+  end
+
   def character_params
-    params.require(:character).permit(:name, :dynasty, :marriage_status, :culture, :religion, :sex)
+    params.require(:character).permit(
+      :id,
+      :name,
+      :dynasty,
+      :marriage_status,
+      :culture,
+      :religion,
+      :sex,
+      character_attribute_params
+    ).tap do |character_params|
+      character_params.require(:character_attribute)
+    end
   end
   
 end
--- ck2-character-builder-backend/app/models/character.rb
+++ ck2-character-builder-backend/app/models/character.rb
@@ -1,3 +1,21 @@
 class Character < ApplicationRecord
   has_one :character_attribute
+
+  before_update do
+    self.character_attribute.save if self.character_attribute
+  end
+
+  class << self
+    def find_or_create_with_attributes(character_params)
+      character = find_by(id: character_params[:id])
+      if character
+        character.character_attribute.assign_attributes(character_params[:character_attribute])
+        character.assign_attributes(character_params.except(:id, :character_attribute))
+        character
+      else
+        Character.new(character_params.except(:id).merge(character_attribute: CharacterAttribute.new(character_params[:character_attribute])))
+      end
+    end
+  end
+
 end
