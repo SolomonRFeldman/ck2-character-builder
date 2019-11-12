90e4dc40fc0444b4e99846994f9c9b92a659a93c
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-11-11 22:28:04 -0500

    sets up traits index route

--- ck2-character-builder-backend/config/routes.rb
+++ ck2-character-builder-backend/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :characters, only: [:index, :show, :create]
   resources :religions, only: [:index]
   resources :cultures, only: [:index]
+  resources :traits, only: [:index]
 end
