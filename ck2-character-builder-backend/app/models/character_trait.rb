caf9933f44aade75103973851f98d4ab8f2557f9
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-11-19 12:34:08 -0500

    fixes trait conflict validation comparison typo

--- ck2-character-builder-backend/app/models/character_trait.rb
+++ ck2-character-builder-backend/app/models/character_trait.rb
@@ -12,7 +12,7 @@ class CharacterTrait < ApplicationRecord
 
   def disallow_opposites_to_join
     if trait.opposites.any? { |opposite_id|
-        character.trait_ids.any? { |trait_id| trait_id = opposite_id }
+        character.trait_ids.any? { |trait_id| trait_id == opposite_id }
       }
       self.errors.add(:trait, 'conflicting traits')
     end
