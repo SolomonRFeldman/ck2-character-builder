a8ec4703e57f54c4ecf8f84dbfe358720cc07c8d
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-11-19 12:15:40 -0500

    validates disallowing opposites trait joins

--- ck2-character-builder-backend/app/models/character_trait.rb
+++ ck2-character-builder-backend/app/models/character_trait.rb
@@ -2,5 +2,16 @@ class CharacterTrait < ApplicationRecord
   belongs_to :character
   belongs_to :trait
 
+  validate :disallow_opposites_to_join
+
+
+
+  def disallow_opposites_to_join
+    if trait.opposites.any? { |opposite_id|
+        character.trait_ids.any? { |trait_id| trait_id = opposite_id }
+      }
+      self.errors.add(:trait, 'conflicting traits')
+    end
+  end
 
 end
