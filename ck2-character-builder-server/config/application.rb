d4f578fb413ca0d580a736804f154271dcde6c17
SolomonRFeldman <SolomonRFeldman@gmail.com>
2019-12-24 14:16:34 -0800

    changes references to server app name to new name

--- ck2-character-builder-server/config/application.rb
+++ ck2-character-builder-server/config/application.rb
@@ -19,7 +19,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Ck2CharacterBuilderBackend
+module Ck2CharacterBuilderServer
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
