3c013c4ff58585de29eebcf42e18ba7b5c7f728d
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-01-08 12:17:27 -0500

    adds ability to provide a character to charCard

--- ck2-character-builder-client/src/components/Character/CharacterCard.js
+++ ck2-character-builder-client/src/components/Character/CharacterCard.js
@@ -2,14 +2,17 @@ import React, { useState } from 'react'
 import CardGroup from 'react-bootstrap/CardGroup'
 import AttributeCard from '../Attribute/AttributeCard'
 import CharacterDetailsCard from './CharacterDetailsCard'
+import Character from '../../models/Character'
 
 export default function CharacterCard(props) {
-  const [character, setCharacter] = useState({})
+  const [character, setCharacter] = useState({...new Character(props.character)})
 
   return(
     <CardGroup className='mx-auto' style={{maxWidth: '800px'}}>
       <CharacterDetailsCard character={character} setCharacter={setCharacter} />
-      <AttributeCard />
+      <AttributeCard character={character} setCharacter={setCharacter} />
     </CardGroup>
   )
 }
+
+CharacterCard.defaultProps = {character: {}}
