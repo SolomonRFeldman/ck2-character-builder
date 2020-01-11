851a4ab0b120daf556f23d53beafae3c914f55fe
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-01-10 20:44:09 -0500

    adds basic religion mock

--- ck2-character-builder-client/src/setupTests.js
+++ ck2-character-builder-client/src/setupTests.js
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom/extend-expect'
+
+const mockReligions = {'Christian': ['Catholic', 'Cathar'], 'Muslim': ['Sunni', 'Zikri']};
+
+jest.spyOn(global, "fetch").mockImplementation(() =>
+  Promise.resolve({
+    json: () => Promise.resolve(mockReligions)
+  })
+);
