380cd7ed6275c592410b080b6b111f9424297eb2
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-01-13 05:08:56 -0500

    prevents closing on select

--- ck2-character-builder-client/src/components/Trait/TraitItem.js
+++ ck2-character-builder-client/src/components/Trait/TraitItem.js
@@ -5,16 +5,14 @@ export default function TraitItem({trait, character, setCharacter}) {
   const path = `../trait_icons/${trait.nameSlug}.png`
   
   const handleClick = () => {
-    console.log(trait)
-    console.log(character)
     setCharacter({...character, traits: [...character.traits, trait], age: character.age + trait.cost})
   }
 
   return(
-    <Dropdown.Item onClick={handleClick}>
+    <a className='dropdown-item' onClick={handleClick}>
       <Image src={path} className='mr-2' /> 
       {trait.name} 
       <div className="float-right ml-2">{trait.cost}</div>
-    </Dropdown.Item>
+    </a>
   )
 }
