7c2132bdee021b82abbabda5cb4c222386376750
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-01-13 08:03:49 -0500

    allows removal of traits

--- ck2-character-builder-client/src/components/Trait/CharacterTrait.js
+++ ck2-character-builder-client/src/components/Trait/CharacterTrait.js
@@ -4,7 +4,21 @@ import { Image } from 'react-bootstrap'
 export default function CharacterTrait({trait, character, setCharacter}) {
   const path = `../trait_icons/${trait.nameSlug}.png`
 
+  const handleClick = () => {
+    const effectiveAttributes = {}
+    Object.keys(character.attributes).forEach(key => {
+      effectiveAttributes[key] = {...character.attributes[key], bonus: character.attributes[key].bonus + (-trait.effects[key] || 0)}
+    })
+    const newTraits = character.traits.filter(charTrait => charTrait.id !== trait.id)
+    setCharacter({
+      ...character,
+      traits: newTraits, 
+      age: character.age - trait.cost, 
+      attributes: effectiveAttributes
+    })
+  }
+
   return(
-    <Image src={path} className='d-inline-block' />
+    <Image onClick={handleClick} src={path} className='d-inline-block' />
   )
 }
