810f11e478c5d8a04107f850360e1a3869b67070
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-01-14 10:56:01 -0500

    makes tooltips mobile friendly

--- ck2-character-builder-client/src/components/Trait/TraitItem.js
+++ ck2-character-builder-client/src/components/Trait/TraitItem.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { Image, OverlayTrigger, Tooltip, Button } from 'react-bootstrap'
 import TraitTooltip from './TraitTooltip'
 
 export default function TraitItem({trait, character, setCharacter}) {
   const path = `../trait_icons/${trait.nameSlug}.png`
   const [hidden, setHidden] = useState(false)
+  const tooltip = useRef(null)
+  const item = useRef(null)
   
   const handleClick = () => {
     const effectiveAttributes = {}
@@ -24,19 +26,36 @@ export default function TraitItem({trait, character, setCharacter}) {
     setHidden(isHidden())
   }, [character.traits])
 
+  const handleTouchMove = () => {
+    tooltip.current.hide()
+    if(document.activeElement) { document.activeElement.blur() }
+  }
+  const handleMouseEnter = () => item.current.parentNode.addEventListener("touchmove", handleTouchMove)
+  const handleMouseLeave = () => item.current.parentNode.removeEventListener("touchmove", handleTouchMove)
+
   return(
     <OverlayTrigger 
-      placement={window.innerWidth > 540 ? 'right' : 'bottom'} 
+      ref={tooltip}
+      placement={window.innerWidth > 540 ? 'right' : 'bottom'}
       popperConfig={{
         modifiers: {
           preventOverflow: {
             enabled: false
           }
         }
-      }} 
+      }}
       overlay={<Tooltip><TraitTooltip trait={trait} /></Tooltip>}
     >
-      <Button className='dropdown-item px-3 py-1' onClick={handleClick} hidden={hidden} style={{overflow: 'visible', touchAction: 'manipulation'}}>
+      <Button 
+        className='dropdown-item px-3 py-1'
+        ref={item}
+        onTouchMove={() => tooltip.current.hide()}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        onClick={handleClick} 
+        hidden={hidden} 
+        style={{overflow: 'visible', touchAction: 'manipulation'}}
+      >
         <Image src={path} className='mr-2' /> 
         {trait.name} 
         <div className="float-right ml-2">{trait.cost}</div>
