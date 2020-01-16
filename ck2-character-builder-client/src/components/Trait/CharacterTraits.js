f99f272f862375c9f2e9f6cbdca559d8cb53bbeb
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-01-16 12:20:39 -0500

    converts vertical scrolling to horizontal

--- ck2-character-builder-client/src/components/Trait/CharacterTraits.js
+++ ck2-character-builder-client/src/components/Trait/CharacterTraits.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import CharacterTrait from './CharacterTrait'
 
 export default function CharacterTraits(props) {
+  const characterTraits = useRef(null)
+  const handleWheel = event => {
+    event.preventDefault()
+    event.target.parentElement.scrollBy(event.deltaY + event.deltaX, 0)
+  }
+
+  useEffect(() => {
+    characterTraits.current.addEventListener('wheel', handleWheel)
+    return () => characterTraits.current.removeEventListener('wheel', handleWheel)
+  }, [])
+
   return(
-    <span className='float-right'
+    <span 
+      ref={characterTraits}
+      className='float-right'
       style={{
         overflowY: 'hidden', 
-        overflowX: 'auto', 
-        width: 'calc(100% - 90px)', 
-        maxHeight: '30px', 
+        overflowX: 'scroll', 
+        width: 'calc(100% - 90px)',
         display: 'inline-block', 
-        whiteSpace: 'nowrap'
+        whiteSpace: 'nowrap',
+        paddingBottom: '20px'
       }}
     >
       {props.character.traits.map(trait => <CharacterTrait key={trait.id} trait={trait} {...props} />)}
