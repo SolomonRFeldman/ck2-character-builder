e16f5a87f2b1360f891455621b5e20ffab9263de
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-01-17 12:33:59 -0500

    removes unused import

--- ck2-character-builder-client/src/_tests_/components/Character/IdentityCard.spec.js
+++ ck2-character-builder-client/src/_tests_/components/Character/IdentityCard.spec.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, fireEvent, within, wait } from '@testing-library/react'
+import { render, fireEvent, within } from '@testing-library/react'
 import CharacterCard from '../../../components/Character/CharacterCard'
 import { act } from 'react-dom/test-utils'
 
