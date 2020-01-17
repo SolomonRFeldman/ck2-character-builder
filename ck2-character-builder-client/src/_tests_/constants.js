cbb877a3667ece028c1eececa5b447242e1ab4d4
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-01-17 18:22:20 -0500

    adds default attributes constant

--- ck2-character-builder-client/src/_tests_/constants.js
+++ ck2-character-builder-client/src/_tests_/constants.js
@@ -1,5 +1,13 @@
 export const DEFAULT_AGE = 16
 
 export const DEFAULT_ATTR = {
-  
+  diplomacy: 5,
+  martial: 5,
+  stewardship: 5,
+  intrigue: 5,
+  learning: 5,
+  health: 5,
+  fertility: 50,
+  sons: 0,
+  daughters: 0
 }
