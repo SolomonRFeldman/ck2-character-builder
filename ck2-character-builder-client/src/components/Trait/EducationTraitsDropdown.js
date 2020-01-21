a6a228423839b55338355b12270d8eff8f265624
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-01-21 18:44:23 -0500

    adds dropdown labels

--- ck2-character-builder-client/src/components/Trait/EducationTraitsDropdown.js
+++ ck2-character-builder-client/src/components/Trait/EducationTraitsDropdown.js
@@ -11,8 +11,8 @@ export default function EducationTraitsDropdown(props) {
   }, [])
 
   return(
-    <Dropdown className='d-inline-block mr-2 float-left'>
-      <Dropdown.Toggle as={Image} src='../trait_icons/add_education.png' />
+    <Dropdown aria-label='Education Dropdown' className='d-inline-block mr-2 float-left'>
+      <Dropdown.Toggle alt='Education Dropdown Toggle' as={Image} src='../trait_icons/add_education.png' />
       <Dropdown.Menu style={{maxHeight: '200px', overflowX: 'hidden', width: '270px'}}>
         {props.traits.map(trait => <EducationItem key={trait.id} trait={trait} windowWidth={windowWidth} {...props} />)}
       </Dropdown.Menu>
