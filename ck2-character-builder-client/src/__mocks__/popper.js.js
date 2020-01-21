9f9f4fd1f2424baad364b3bf821a1e3e0f98ad23
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-01-21 13:56:22 -0500

    adds popper css fix

---
+++ ck2-character-builder-client/src/__mocks__/popper.js.js
@@ -0,0 +1,13 @@
+
+const PopperJS = jest.requireActual('popper.js');
+
+export default class {
+  static placements = PopperJS.placements;
+
+  constructor() {
+    return {
+      destroy: () => {},
+      scheduleUpdate: () => {}
+    };
+  }
+};
