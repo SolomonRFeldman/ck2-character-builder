a6dc84cc38da46b05883be060a6e58849df37e2a
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-01-21 16:34:29 -0500

    properly unslugs effect keys and spaces colen

--- ck2-character-builder-client/src/components/Trait/TraitTooltip.js
+++ ck2-character-builder-client/src/components/Trait/TraitTooltip.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 export default function TraitTooltip({trait}) {
+  const unslug = string => {
+    const words = string.split('_')
+    return words.map(word => word[0].toUpperCase() + word.slice(1)).join(' ')
+  }
   return(
     <React.Fragment>
       <h4>{trait.name}:</h4>
@@ -8,7 +12,7 @@ export default function TraitTooltip({trait}) {
       <p>{trait.description}</p>
       <ul>
         {Object.keys(trait.effects).map(attr => {
-          return <div key={attr} className="ml-n5">{attr[0].toUpperCase() + attr.slice(1)} : {trait.effects[attr]}</div>
+          return <div key={attr} className="ml-n5">{unslug(attr)}: {trait.effects[attr]}</div>
         })}
       </ul>
     </React.Fragment>
