5647562a69ed83ae5eb49dcb211d17434b02f7a8
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-01-22 16:15:32 -0500

    adds character edcation tooltip

--- ck2-character-builder-client/src/components/Trait/CharacterEducation.js
+++ ck2-character-builder-client/src/components/Trait/CharacterEducation.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import { Image } from 'react-bootstrap'
+import { Image, OverlayTrigger, Tooltip } from 'react-bootstrap'
+import TraitTooltip from './TraitTooltip'
 
 export default function CharacterEducation({character}) {
   const path = `../trait_icons/${character.education.nameSlug}.png`
 
   return(
-    <Image alt={`${character.education.name} Character Education`} src={path} className='d-inline-block' />
+    <OverlayTrigger
+      popperConfig={{
+        modifiers: {
+          preventOverflow: { enabled: false },
+          hide: { enabled: false }
+        }
+      }} 
+      placement='bottom' 
+      overlay={<Tooltip aria-label={`${character.education.name} Trait Tooltip`}><TraitTooltip trait={character.education} /></Tooltip>}
+    >
+      <Image alt={`${character.education.name} Character Education`} src={path} className='d-inline-block' />
+    </OverlayTrigger>
   )
 }
