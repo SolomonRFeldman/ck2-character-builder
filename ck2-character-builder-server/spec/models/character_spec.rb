aa06dfe2e2863170b8cb75504c173b5879af1504
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-01-23 19:49:31 -0500

    adds test for trait serialization

--- ck2-character-builder-server/spec/models/character_spec.rb
+++ ck2-character-builder-server/spec/models/character_spec.rb
@@ -26,6 +26,25 @@ RSpec.describe Character, :type => :model do
     }
   end
 
+  let(:valid_trait) do
+    {
+      name: "Kind",
+      description: <<~DESC.strip,
+        This character is kind and full of empathy. \
+        The sixth virtue, 'Humanitas', is popular with vassals, but makes for a rather poor Spymaster.
+      DESC
+      cost: 5,
+      effects: {
+        intrigue: -2,
+        diplomacy: 2,
+        personal_combat_skill: -5,
+        vassal_opinion: 5,
+        same_trait_opinion: 5,
+        opposite_trait_opinion: -5
+      }
+    }
+  end
+
   it "is valid with a name, dynasty, marriage_status, religion, culture, and sex" do
     expect(Character.create(valid_character)).to be_valid
   end
@@ -34,6 +53,7 @@ RSpec.describe Character, :type => :model do
     before do
       character = Character.create(valid_character)
       character.character_attribute = CharacterAttribute.create(valid_character_attribute)
+      character.traits = [Trait.create(valid_trait)]
     end
 
     it "can be serialized" do
@@ -47,5 +67,10 @@ RSpec.describe Character, :type => :model do
     it "has one attribute set" do
       expect(Character.all.last.character_attribute).to eq(CharacterAttribute.all.last)
     end
+
+    it "can serialize its traits" do
+      expect(CharacterSerializer.new(Character.all.last).to_serialized_json).to include(Trait.all.first.to_json)
+    end
+
   end
 end
