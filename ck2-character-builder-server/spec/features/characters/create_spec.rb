61656e24cf437986d9c46757b1860bd88c5e8ed1
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-01-25 12:00:08 -0500

    adds tests for creating with traits

--- ck2-character-builder-server/spec/features/characters/create_spec.rb
+++ ck2-character-builder-server/spec/features/characters/create_spec.rb
@@ -24,9 +24,61 @@ describe 'Character Features', :type => :feature do
     }
   end
 
-  context "when a post request for a new character is sent with an attribute set" do
+  let(:valid_education) do
+    Education.create({
+      name: "Amateurish Plotter",
+      description: <<~DESC.strip,
+        The Amateurish Plotter has received an education emphasizing intrigue skills. Unfortunately, it didn't stick.
+      DESC
+      cost: 0,
+      effects: {
+        intrigue: 1,
+        stewardship: -1,
+        personal_combat_skill: 4
+      }
+    })
+  end
+
+  let(:valid_trait) do
+    Trait.create({
+      name: "Kind",
+      description: <<~DESC.strip,
+        This character is kind and full of empathy. \
+        The sixth virtue, 'Humanitas', is popular with vassals, but makes for a rather poor Spymaster.
+      DESC
+      cost: 5,
+      effects: {
+        intrigue: -2,
+        diplomacy: 2,
+        personal_combat_skill: -5,
+        vassal_opinion: 5,
+        same_trait_opinion: 5,
+        opposite_trait_opinion: -5
+      }
+    })
+  end
+
+  let(:secondary_trait) do
+    Trait.create({
+      name: "Hunter",
+      description: <<~DESC.strip,
+        This character only feels truly alive when killing God's little creatures.
+      DESC
+      cost: 10,
+      effects: {
+        pursuit: 20,
+        martial: 2,
+        diplomacy: 1,
+        personal_combat_skill: 10,
+        same_trait_opinion: 10
+      }
+    })
+  end
+
+  context "when a post request for a new character is sent" do
     before do
-      page.driver.submit :post, characters_path, character: valid_character
+      character_traits = { education_id: valid_education.id }
+      page.driver.submit :post, characters_path, character: valid_character.merge(character_traits), character_trait_ids: [valid_trait.id, secondary_trait.id]
     end
 
     it "creates the character" do
@@ -40,6 +92,15 @@ describe 'Character Features', :type => :feature do
     it "creates a character with an attribute set" do
       expect(Character.all.last.character_attribute).to_not be_nil
       expect(Character.all.last.character_attribute).to eq(CharacterAttribute.all.last)
+    end
+
+    it "creates a character with an education" do
+      expect(Character.all.last.education).to eq(valid_education)
+    end
+
+    it "creates a character with traits" do
+      expect(Character.all.last.traits).to include(valid_trait)
+      expect(Character.all.last.traits).to include(secondary_trait)
     end
   end
 
