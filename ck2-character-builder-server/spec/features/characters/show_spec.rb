f7526500fa71a1730e2e04eefa6ee6d5481d560b
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-01-25 13:28:51 -0500

    includes char traits in tests

--- ck2-character-builder-server/spec/features/characters/show_spec.rb
+++ ck2-character-builder-server/spec/features/characters/show_spec.rb
@@ -25,8 +25,60 @@ describe 'Character Features', :type => :feature do
     }
   end
 
+  let(:valid_education) do
+    Education.create({
+      name: "Amateurish Plotter",
+      description: <<~DESC.strip,
+        The Amateurish Plotter has received an education emphasizing intrigue skills. Unfortunately, it didn't stick.
+      DESC
+      cost: 0,
+      effects: {
+        intrigue: 1,
+        stewardship: -1,
+        personal_combat_skill: 4
+      }
+    })
+  end
+
+  let(:valid_trait) do
+    Trait.create({
+      name: "Kind",
+      description: <<~DESC.strip,
+        This character is kind and full of empathy. \
+        The sixth virtue, 'Humanitas', is popular with vassals, but makes for a rather poor Spymaster.
+      DESC
+      cost: 5,
+      effects: {
+        intrigue: -2,
+        diplomacy: 2,
+        personal_combat_skill: -5,
+        vassal_opinion: 5,
+        same_trait_opinion: 5,
+        opposite_trait_opinion: -5
+      }
+    })
+  end
+
+  let(:secondary_trait) do
+    Trait.create({
+      name: "Hunter",
+      description: <<~DESC.strip,
+        This character only feels truly alive when killing God's little creatures.
+      DESC
+      cost: 10,
+      effects: {
+        pursuit: 20,
+        martial: 2,
+        diplomacy: 1,
+        personal_combat_skill: 10,
+        same_trait_opinion: 10
+      }
+    })
+  end
+
   before do
-    page.driver.submit :post, characters_path, character: valid_character
+    character_traits = { education_id: valid_education.id, trait_ids: [valid_trait.id, secondary_trait.id] }
+    page.driver.submit :post, characters_path, character: valid_character.merge(character_traits)
   end
 
   context "when a get request is sent to /characters" do
