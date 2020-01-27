7b94ff56fb3fcf29669396aebe39970396ca6f4e
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-01-27 17:08:06 -0500

    checks for CharAttr params before trying to update it

--- ck2-character-builder-server/app/models/character.rb
+++ ck2-character-builder-server/app/models/character.rb
@@ -26,7 +26,11 @@ class Character < ApplicationRecord
     def find_or_create_with_attributes(character_params)
       character = find_by(id: character_params[:id])
       if character
-        character.character_attribute.assign_attributes(character_params[:character_attribute])
+        if character_params[:character_attribute]
+          character.character_attribute ? 
+          character.character_attribute.assign_attributes(character_params[:character_attribute]) :
+          character.character_attribute = CharacterAttribute.new(character_params[:character_attribute])
+        end
         character.assign_attributes(character_params.except(:id, :character_attribute))
         character
       else
