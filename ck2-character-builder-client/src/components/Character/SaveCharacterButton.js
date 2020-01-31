7cf1f62265efebbf0b92b373dfe5cb5254f03505
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-01-31 11:29:58 -0500

    cleans up unnecessary/confusing code

--- ck2-character-builder-client/src/components/Character/SaveCharacterButton.js
+++ ck2-character-builder-client/src/components/Character/SaveCharacterButton.js
@@ -3,6 +3,16 @@ import { Button } from 'react-bootstrap'
 
 export default function SaveCharacterButton({className, character, setCharacter, characters, setCharacters, setSelectedChar}){
   const handleClick = () => {
+    const preparedChar = {
+      ...character,
+      character_attribute: Object.keys(character.attributes).reduce((result, attr) => {
+        result[attr] = character.attributes[attr].base
+        return result
+      }, {}),
+      education_id: character.education.id,
+      trait_ids: character.traits.map(trait => trait.id)
+    }
+
     const obj = {
       method: "POST",
       headers: {
@@ -10,28 +20,19 @@ export default function SaveCharacterButton({className, character, setCharacter,
         "Accept": "application/json"
       },
       body: JSON.stringify({
-        character: {
-          ...character,
-          character_attribute: Object.keys(character.attributes).reduce((result, attr) => {
-            result[attr] = character.attributes[attr].base
-            return result
-          }, {}),
-          education_id: character.education.id,
-          trait_ids: character.traits.map(trait => trait.id)
-        }
+        character: preparedChar
       })
     }
+
     fetch('/characters', obj).then(resp => resp.json()).then(json => {
       if(json.errors && Object.entries(json.errors).length > 0) {
         setCharacter({ ...character, errors: json.errors.character })
       } else {
         setCharacter({...character, id: json.id, errors: {} })
-        const characterIndex = characters.findIndex(charToUpdate => charToUpdate.id === json.id)
-        let newCharacters
-        if(characterIndex !== -1) {
-          characters.splice(characterIndex, 1)
-        }
-        newCharacters = [json, ...characters]
+
+        const characterIndex = characters.findIndex(charToRemove => charToRemove.id === json.id)
+        if(characterIndex !== -1) { characters.splice(characterIndex, 1) }
+        const newCharacters = [json, ...characters]
         setCharacters(newCharacters)
         setSelectedChar(json.id)
       }
