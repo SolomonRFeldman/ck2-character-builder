e68975b953939be3487b52c4e7c3a81927c652c3
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-01-31 22:10:15 -0500

    fixes character load select from wrapping

--- ck2-character-builder-client/src/components/Character/LoadCharacterSelect.js
+++ ck2-character-builder-client/src/components/Character/LoadCharacterSelect.js
@@ -4,7 +4,7 @@ import { Form } from 'react-bootstrap'
 export default function LoadCharacterSelect({characters, selectedChar, setSelectedChar}){
   const handleChange = event => setSelectedChar(event.target.value)
   return(
-    <Form>
+    <Form style={{maxWidth: 'calc(100% - 70px)'}}>
       <Form.Control aria-label='Load Character Select' onChange={handleChange} value={selectedChar} as="select">
         {characters.map(character => <option key={character.id} value={character.id}>{character.name} {character.dynasty}</option>)}
       </Form.Control>
