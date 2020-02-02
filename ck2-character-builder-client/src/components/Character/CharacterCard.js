5378a828d228f55d834d4ddf653251ce650149db
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-02 14:40:13 -0500

    abstracts inline style to css sheet

--- ck2-character-builder-client/src/components/Character/CharacterCard.js
+++ ck2-character-builder-client/src/components/Character/CharacterCard.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './CharacterCard.css'
 import CardGroup from 'react-bootstrap/CardGroup'
 import AttributeCard from '../Attribute/AttributeCard'
 import CharacterDetailsCard from './CharacterDetailsCard'
@@ -8,7 +9,7 @@ export default function CharacterCard(props) {
   const [character, setCharacter] = useState({...new Character(props.character)})
 
   return(
-    <CardGroup className='mx-auto my-4' style={{maxWidth: '800px'}}>
+    <CardGroup className='mx-auto my-4 character-card'>
       <CharacterDetailsCard character={character} setCharacter={setCharacter} />
       <AttributeCard character={character} setCharacter={setCharacter} />
     </CardGroup>
