8e5cb50e7fabf64a698d42ba70457c6ad260d1d7
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-02 22:12:26 -0500

    adds pageview tracker

--- ck2-character-builder-client/src/index.js
+++ ck2-character-builder-client/src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import ReactGA from 'react-ga';
 
 ReactGA.initialize('UA-157527332-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
