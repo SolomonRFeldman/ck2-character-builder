a80ea4ebab032e72fb72b566bb426773cfbefc39
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-02 21:55:27 -0500

    adds google analytics

--- ck2-character-builder-client/src/index.js
+++ ck2-character-builder-client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-157527332-1');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
