2d5044a371b0fc602d24023db5f47e96f910a128
SolomonRFeldman <SolomonRFeldman@gmail.com>
2020-02-02 19:24:07 -0500

    adds retrying fetch till success

---
+++ ck2-character-builder-client/src/fetch.js
@@ -0,0 +1,18 @@
+export function fetchUntilSuccess(path, options) {
+  return new Promise ((resolve, reject) => {
+    let retryCount = 0
+    const fetchTry = () => fetch(path, options).then(response => { 
+      retryCount += 1
+      if(response.status === 200) {
+        resolve(response)
+      } else {
+        if(retryCount >= 10) {
+          reject(response)
+        } else {
+          setTimeout(fetchTry, 1000)
+        }
+      }
+    })
+    fetchTry()
+  })
+}
